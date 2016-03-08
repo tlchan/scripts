b7f71813077910da28b97ac02443a92d6048b4d8
Tony Chan <tlchan@gmail.com>
2016-03-08 12:00:02 -0800

    updated, using FQDN

--- iDRAC/check_all_bootorder2.sh
+++ iDRAC/check_all_bootorder2.sh
@@ -4,72 +4,38 @@
 
 USER=root
 PASS=calvin
-SSHOPTS="-q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+SSHOPTS="-q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=10"
 
 function racadm_cmd () 
 {
   # $1 row; $2 rack; $3 type; $4 start; $5 stop
   for i in `seq $4 $5`
   do
-    bootorder=$(sshpass -p $PASS ssh $SSHOPTS $USER@cnlvr${1}r${2}${3}${i}-oob.lv.cncf.io \
+    FQDN="cnlvr${1}r${2}${3}${i}-oob.lv.cncf.io"
+    IPADDR=`ping -c1 $FQDN | grep from | cut -d ' ' -f 4`
+    bootorder=$(sshpass -p $PASS ssh $SSHOPTS $USER@$FQDN \
         racadm get BIOS.BiosBootSettings.bootseq | grep BootSeq=)
-    echo "cnlvr${1}r${2}${3}${i}-oob : $bootorder"
+    echo "[$FQDN --> $IPADDR ] $bootorder"
   done
 }
 
 
-echo "##### row 01 rack 01 #####" 
-racadm_cmd 01 01 c 1 36
+for i in 01 02 03 04 05 06; do
+  echo "##### row 01 rack $i #####" 
+  racadm_cmd 01 $i c 1 36
+done 
 
-echo "##### row 01 rack 02 #####" 
-racadm_cmd 01 02 c 1 36
+for i in 07 08 09 10; do
+  echo "##### row 01 rack $i #####" 
+  racadm_cmd 01 $i s 1 19
+done 
 
-echo "##### row 01 rack 03 #####"
-racadm_cmd 01 03 c 1 36
+for i in 01 02 03 04 05 06 07; do
+  echo "##### row 02 rack $i #####"
+  racadm_cmd 02 $i c 1 36
+done
 
-echo "##### row 01 rack 04 #####"
-racadm_cmd 01 04 c 1 36
-
-echo "##### row 01 rack 06 #####" 
-racadm_cmd 01 06 c 1 36
-
-echo "##### row 01 rack 07 #####" 
-racadm_cmd 01 07 s 1 19
-
-echo "##### row 01 rack 08 #####" 
-racadm_cmd 01 08 s 1 19
-
-echo "##### row 01 rack 09 #####"
-racadm_cmd 01 09 s 1 19
-
-echo "##### row 01 rack 10 #####" 
-racadm_cmd 01 10 s 1 19
-
-##
-
-echo "##### row 02 rack 01 #####" 
-racadm_cmd 02 01 c 1 36
-
-echo "##### row 02 rack 02 #####" 
-racadm_cmd 02 02 c 1 36
-
-echo "##### row 02 rack 03 #####" 
-racadm_cmd 02 03 c 1 36
-
-echo "##### row 02 rack 04 #####" 
-racadm_cmd 02 04 c 1 36
-
-echo "##### row 02 rack 06 #####" 
-racadm_cmd 02 06 c 1 36
-
-echo "##### row 02 rack 07 #####" 
-racadm_cmd 02 07 s 1 19
-
-echo "##### row 02 rack 08 #####" 
-racadm_cmd 02 08 s 1 19
-
-echo "##### row 02 rack 09 ####"
-racadm_cmd 02 09 s 1 19
-
-echo "##### row 02 rack 10 ####"
-racadm_cmd 02 10 s 1 19"
+for i in 08 09 10; do
+  echo "##### row 02 rack $i #####"
+  racadm_cmd 02 $i s 1 19
+done
